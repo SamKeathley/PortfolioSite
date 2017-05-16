0ad7ca69f538ff8f377a82cd4f5cbe794a3898f9
Samantha Keathley <samncollette@gmail.com>
2017-05-16 13:19:04 -0500

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,21 +4,14 @@ import '../css/Footer.css';
 class Footer extends Component {
   render(){
     return(
-      <div className="fa-footer">
-        <footer className="footer">
-          <div className="content-wrapper pull-right fixed-bottom">
-            <ul className="icons">
-            <li><i className="fa fa-github-alt" aria-hidden="true"></i>
-  </li>
-              <li><i className="fa fa-medium" aria-hidden="true"></i></li>
-              <li><i className="fa fa-linkedin" aria-hidden="true"></i>
-  </li>
-              <li><i className="fa fa-instagram" aria-hidden="true"></i>
-  </li>
-            </ul>
-          </div>
-        </footer>
-      </div>
+      <footer className="footer">
+        <ul className="icons">
+          <li><i className="fa fa-github-alt" aria-hidden="true"></i></li>
+          <li><i className="fa fa-medium" aria-hidden="true"></i></li>
+          <li><i className="fa fa-linkedin" aria-hidden="true"></i></li>
+          <li><i className="fa fa-instagram" aria-hidden="true"></i></li>
+        </ul>
+      </footer>
     )
   }
 }
