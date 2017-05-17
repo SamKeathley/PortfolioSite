9b65ae033b8f63b76588b8c60d30119523e9eee6
Samantha Keathley <samncollette@gmail.com>
2017-05-17 12:09:59 -0500

    Changed font color and pointed out resume

--- src/components/About.js
+++ src/components/About.js
@@ -20,10 +20,9 @@ class About extends Component {
             Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.
             <br />
             <br />
-            You can view my resume here! Please don't hesitate to reach out with any questions.
+            <a href={Resume} className="resume"><i className="fa fa-file fa-lg" aria-hidden="true"></i></a>
             <br />
-            <br />
-            <a href={Resume} className="resume"><i className="fa fa-file fa-lg" aria-hidden="true"></i></a> 
+            (Resume)
             </p>
           </div>
         </div>
